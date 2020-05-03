ebf0972d27f88a3ced7146405b2e22f3453a2d96
rodi2000 <rohanymodi@gmail.com>
2020-05-03 15:52:47 +0530

    required

--- lib/screens/home/detailpage.dart
+++ lib/screens/home/detailpage.dart
@@ -15,24 +15,61 @@ class DetailPage extends StatelessWidget {
         centerTitle: true,
         backgroundColor: Colors.cyan,
       ),
-      body: ListView(
-        children: <Widget>[
-          Container(
-            height: 300,
-            child: GridTile(
-                child: Container(
-                  color: Colors.white,
-                  child: Image.network(
-                    mobile.url,
-                    loadingBuilder: (context, child, progress) {
-                      return progress == null ? child : LinearProgressIndicator();
-                    },
-                  ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Container(
+              height: 300,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: NetworkImage(mobile.url),
+                  fit: BoxFit.contain,
                 ),
+              ),
             ),
-          )
-        ],
+            Container(
+              height: 40,
+              width: MediaQuery.of(context).size.width,
+              color: Colors.black,
+              child: Center(
+                child: Text('Specifications', style: TextStyle(fontSize: 20, color: Colors.white)),
+              ),
+            ),
+           Container(
+             height: 340,
+              child: ListView(
+                shrinkWrap: true,
+                children: <Widget>[
+                  createListTile('Name', mobile.name),
+                  createListTile('Company Name',mobile.companyName),
+                  createListTile('Operating System',mobile.operatingSystem),
+                  createListTile('Battery',mobile.battery),
+                  createListTile('Screen Size',mobile.screenSize),
+                  createListTile('Front Camera',mobile.frontCamera),
+                  createListTile('Back Camera',mobile.backCamera),
+                  createListTile('Ram',mobile.ram),
+                  createListTile('Rom',mobile.rom),
+                  createListTile('Processor',mobile.processor),
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
+}
+
+Widget createListTile (String description, String value) {
+  return Column(
+    children: <Widget>[
+      ListTile(
+        dense: true,
+        title: Text('$description : $value',
+          style: TextStyle(fontWeight: FontWeight.bold,fontSize: 18.0),
+        ),
+      ),
+      Divider(color: Colors.black),
+    ],
+  );
 }
